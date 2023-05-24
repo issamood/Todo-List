ff95199cc2fa640b1f5976a0dcb22d1b77a712a6
Dan <dxkim201@gmail.com>
2023-05-23 22:01:51 -0400

    Added task window opening and closing, when creating new task

--- src/loadContent.js
+++ src/loadContent.js
@@ -11,14 +11,42 @@ export default function loadContent(container){
 
     const add = document.createElement('div');
     add.classList.add('add');
-    const taskWindow = document.createElement('div');
-    taskWindow.classList.add('taskWindow');
 
+    const taskWindow = document.createElement('div');
+    const taskWindowContent = document.createElement('div');
+    const taskContentHeader = document.createElement('h1');
+    const taskContentTitle = document.createElement('div');
+    const taskClose = document.createElement('button');
+    taskClose.setAttribute('id','closeTaskWindow');
+    taskClose.innerHTML = 'Close';
+    taskClose.addEventListener('click', () => {
+        taskWindow.classList.remove('show');
+    })
+
+    taskContentHeader.classList.add('taskContentHeader');
+    taskContentHeader.innerHTML = 'Add Task';
+
+    taskContentTitle.classList.add('taskContentTitle');
+    taskContentTitle.innerHTML = 'Enter task title:';
+
+    taskWindow.classList.add('taskWindow');
+    window.addEventListener('click', (event) => {
+        if (event.target == taskWindow) {
+            taskWindow.classList.remove('show');
+        }
+    })
+    taskWindowContent.classList.add('taskWindowContent');
+
+    taskWindowContent.appendChild(taskContentHeader);
+    taskWindowContent.appendChild(taskContentTitle);
+    taskWindowContent.appendChild(taskClose);
+    taskWindow.appendChild(taskWindowContent);
     content.appendChild(taskWindow);
     add.addEventListener('click', () => {
-        
+        taskWindow.classList.add('show');
 
     });
+    
 
     const addIcon = document.createElement('div');
     addIcon.classList.add('addIcon');
